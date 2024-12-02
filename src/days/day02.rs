756a66eea489dd6d0adc9b85ab0a98456d412f7a
Avery Black <avery.c.black@gmail.com>
2024-12-01 22:16:52 -0800

    Day 3

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,12 +1,35 @@
 use crate::{Solution, SolutionPair};
 
-///////////////////////////////////////////////////////////////////////////////
+fn is_safe(r: &Vec<i32>) -> bool {
+    r.is_sorted_by(|&a, &b| i32::abs_diff(a, b) <= 3 && a < b) ||
+    r.is_sorted_by(|&a, &b| i32::abs_diff(a, b) <= 3 && b < a)
+}
 
 pub fn solve(str: String) -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
-    let _ = str;
+
+    let reports: Vec<Vec<_>> = str
+        .lines()
+        .map(|l| l.split_ascii_whitespace().map(|num| num.parse::<i32>().unwrap()).collect())
+        .collect();
+
+    let sol1 = reports
+        .iter()
+        .filter(|&r| is_safe(r))
+        .count();
+
+    let sol2 = reports
+        .iter()
+        .filter(|&r| {
+            for i in 0..r.len() {
+                let r_p = [&r[0..i], &r[(i + 1)..]].concat();
+                if is_safe(&r_p) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .count();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(is_sorted)]
+
 mod days;
 mod etc;
 
