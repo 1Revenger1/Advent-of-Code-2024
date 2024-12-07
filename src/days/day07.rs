3e3ab2d55554dca9e83708f2201599616daf3a28
Avery Black <avery.c.black@gmail.com>
2024-12-06 22:10:19 -0800

    Day 7 but with recursion

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,6 +1,36 @@
 use crate::{Solution, SolutionPair};
 
-///////////////////////////////////////////////////////////////////////////////
+fn p1(ops: &[u64], res: u64, target: u64) -> bool {
+    if ops.is_empty() {
+        return res == target;
+    }
+
+    let add = ops.first().unwrap() + res;
+    let mult = ops.first().unwrap() * res;
+    p1(&ops[1..], add, target) || p1(&ops[1..], mult, target)
+}
+
+fn p2(ops: &[u64], res: u64, target: u64) -> bool {
+    if ops.is_empty() {
+        return res == target;
+    }
+
+    let add = ops[0] + res;
+    let mult = ops[0] * res;
+    let concat = (res * cheap_log10(ops[0])) + ops[0];
+    p2(&ops[1..], add, target) ||
+        p2(&ops[1..], mult, target) ||
+        p2(&ops[1..], concat, target)
+}
+
+fn cheap_log10(v: u64) -> u64 {
+    if v < 10 {
+        return 10;
+    } else if v < 100 {
+        return 100;
+    }
+    return 1000;
+}
 
 pub fn solve(str: String) -> SolutionPair {
     let equations: Vec<(u64, Vec<u64>)> = str
@@ -15,47 +45,24 @@ pub fn solve(str: String) -> SolutionPair {
 
     let sol1: u64 = equations
         .iter()
-        .map(|(val, operands)| {
-            let num_operators = operands.len() - 1;
-            for perm in 0..=((1 << num_operators) - 1) {
-                let mut res = operands[0];
-                for (bit, v) in operands.iter().skip(1).enumerate() {
-                    if (perm & (1 << bit)) != 0 {
-                        res *= *v;
-                    } else {
-                        res += v;
-                    }
-                }
-
-                if res == *val {
-                    return *val;
-                }
+        .map(|(target, operands)| {
+            if p1(&operands[1..], operands[0], *target) {
+                return *target;
+            } else {
+                return 0;
             }
-            0
         })
         .sum();
 
+    
     let sol2: u64 = equations
         .iter()
-        .map(|(val, operands)| {
-            let num_operators = operands.len() - 1;
-            'next_perm: for perm in 0..=((1 << (num_operators * 2)) - 1) {
-                let mut res = operands[0];
-                for (bit, v) in operands.iter().skip(1).enumerate() {
-                    let op = (perm >> (bit * 2)) & 0x3;
-                    match op {
-                        0 => res += v,
-                        1 => res *= *v,
-                        2 => res = (res * 10_u64.pow(v.ilog10() + 1)) + *v,
-                        _ => continue 'next_perm
-                    }
-                }
-
-                if res == *val {
-                    return *val;
-                }
+        .map(|(target, operands)| {
+            if p2(&operands[1..], operands[0], *target) {
+                return *target;
+            } else {
+                return 0;
             }
-            0
         })
         .sum();
 
